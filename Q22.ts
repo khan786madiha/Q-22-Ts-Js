830a66a643617762e5dc5fe2d9c7b4a35fbfabf0
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-03-05 10:41:53 -0800

    Q-22 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-22

---
+++ Q22.ts
@@ -0,0 +1,9 @@
+// 22.	Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+const fruits: string[] = ["Mango", "Banana", "Apple", "Orange", "Pineapple", "guava", "Grapes"]
+
+console.log(fruits[0]);
+console.log(fruits[7]);
+console.log(fruits[6]);
+
+
